ae9987acca444afa225c3d34fe84f971ae49b9c4
SmashKap <ankaplan@outlook.com>
2018-05-07 11:30:47 -0500

    Added custom class to page.php

--- wp-content/themes/twentyseventeen-child/page.php
+++ wp-content/themes/twentyseventeen-child/page.php
@@ -17,13 +17,13 @@
 
 get_header(); ?>
 
-<div class="wrap">
+<div class="wrap wrap-ank">
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
 			<?php
 			while ( have_posts() ) : the_post();
-			
+
 
 				get_template_part( 'template-parts/page/content', 'page' );
 
